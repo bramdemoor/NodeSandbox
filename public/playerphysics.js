26288b9adf574009720e44c8ffd1d101ea2449c8
Bram De Moor <bram.demoor@gmail.com>
2013-01-06 21:30:59 +0100

    Movement fix (physics)

--- public/playerphysics.js
+++ public/playerphysics.js
@@ -10,8 +10,8 @@ $(function() {
         var MAX_SPEED_VERTICAL = 10;
         var OFFSET_X = 5;
         var OFFSET_Y = 2;
-        var CHARACTER_WIDTH = 16;
-        var CHARACTER_HEIGHT = 16;
+        var CHARACTER_WIDTH_HALF = 16;
+        var CHARACTER_HEIGHT_HALF = 16;
         var COLISSION_CORRECTION = 0.01;
         var GRAVITY = 2;
         var JUMP_FORCE = 5;
@@ -21,8 +21,6 @@ $(function() {
         // Calculation variables
         self.currentX = 0;
         self.currentY = 0;
-        self.spriteWidth = 0;
-        self.spriteHeight = 0;
         self.goalx = 0;
         self.goaly = 0;
         self.incx = 0;
@@ -39,12 +37,12 @@ $(function() {
 
         /// Get the X-result of our calculation. To be called after update.
         self.getX = function() {
-            return dest.x - OFFSET_X;
+            return self.dest.x - OFFSET_X;
         };
 
         /// Get the Y-result of our calculation. To be called after update.
         self.getY = function() {
-            return dest.y - OFFSET_Y;
+            return self.dest.y - OFFSET_Y;
         };
 
         self.hitTest = function(x,y) {
@@ -76,7 +74,7 @@ $(function() {
             // Not walking? Keep current. Otherwise, switch direction.
             self.goalDir = self.goalx == 0 ? 0 : (self.goalx > 0 ? 1 : -1);
 
-            self.player.onWantsToMove(self.goalx);
+            self.player.onWantsToMove();
 
             self.incx += self.goalx + self.horizontalFriction;
 
@@ -93,53 +91,56 @@ $(function() {
         };
 
         self.handleCollisions = function() {
+
             while (true) {
 
-                self.centerx = dest.x - self.currentX - OFFSET_X;
-                self.centery = dest.y - self.currentY - OFFSET_Y;
+                self.boundary = 0;
+                self.crossing = 0;
+                self.centerx = self.dest.x - self.currentX - OFFSET_X;
+                self.centery = self.dest.y - self.currentY - OFFSET_Y;
 
                 if (self.wantsToMoveRight()) {
 
-                    self.boundary = Math.floor(dest.right / CHARACTER_WIDTH) * CHARACTER_WIDTH;
-                    self.crossing = (dest.right - self.boundary) / self.centerx * self.centery + dest.y;
+                    self.boundary = Math.floor(self.dest.right / CHARACTER_WIDTH_HALF) * CHARACTER_WIDTH_HALF;
+                    self.crossing = (self.dest.right - self.boundary) / self.centerx * self.centery + self.dest.y;
 
                     if (self.hitTestRight()) {
                         self.incx = 0;
-                        dest.x = self.boundary - dest.width - COLISSION_CORRECTION;
+                        self.dest.x = self.boundary - self.dest.width - COLISSION_CORRECTION;
                         self.player.onCollideRight();
                         continue;
                     }
 
                 } else if (self.wantsToMoveLeft()) {
 
-                    self.boundary = Math.floor(dest.x / CHARACTER_WIDTH) * CHARACTER_WIDTH + CHARACTER_WIDTH;
-                    self.crossing = (self.boundary - dest.x) / self.centerx * self.centery + dest.y;
+                    self.boundary = Math.floor(self.dest.x / CHARACTER_WIDTH_HALF) * CHARACTER_WIDTH_HALF + CHARACTER_WIDTH_HALF;
+                    self.crossing = (self.boundary - self.dest.x) / self.centerx * self.centery + self.dest.y;
 
                     if (self.hitTestLeft()) {
                         self.incx = 0;
-                        dest.x = self.boundary + COLISSION_CORRECTION;
+                        self.dest.x = self.boundary + COLISSION_CORRECTION;
                         self.player.onCollideLeft();
                         continue;
                     }
                 }
                 if (self.wantsToMoveDown()) {
 
-                    self.boundary = Math.floor(dest.bottom / CHARACTER_HEIGHT) * CHARACTER_HEIGHT;
-                    self.crossing = (dest.bottom - self.boundary) / self.centery * self.centerx + dest.x;
+                    self.boundary = Math.floor(self.dest.bottom / CHARACTER_HEIGHT_HALF) * CHARACTER_HEIGHT_HALF;
+                    self.crossing = (self.dest.bottom - self.boundary) / self.centery * self.centerx + self.dest.x;
                     if (self.hitTestBottom()) {
                         self.jumping = false;
                         self.incy = 0;
-                        dest.y = self.boundary - dest.height - COLISSION_CORRECTION;
+                        self.dest.y = self.boundary - self.dest.height - COLISSION_CORRECTION;
                         self.player.onCollideFloor(self.crossing, self.boundary);
                         continue;
                     }
                 } else if (self.wantsToMoveUp()) {
 
-                    self.boundary = Math.floor(dest.y / CHARACTER_HEIGHT) * CHARACTER_HEIGHT + CHARACTER_HEIGHT;
-                    self.crossing = (self.boundary - dest.y) / self.centery * self.centerx + dest.x;
+                    self.boundary = Math.floor(self.dest.y / CHARACTER_HEIGHT_HALF) * CHARACTER_HEIGHT_HALF + CHARACTER_HEIGHT_HALF;
+                    self.crossing = (self.boundary - self.dest.y) / self.centery * self.centerx + self.dest.x;
                     if (self.hitTestTop()) {
                         self.incy = 0;
-                        dest.y = self.boundary + COLISSION_CORRECTION;
+                        self.dest.y = self.boundary + COLISSION_CORRECTION;
                         self.player.onCollideTop();
                         continue;
                     }
@@ -170,59 +171,59 @@ $(function() {
 
         self.hitTestRight = function () {
             return (
-                (self.hitTest(self.boundary, self.crossing) && !self.hitTest(self.boundary - CHARACTER_WIDTH, self.crossing))
+                (self.hitTest(self.boundary, self.crossing) && !self.hitTest(self.boundary - CHARACTER_WIDTH_HALF, self.crossing))
                 ||
-                (self.hitTest(self.boundary, self.crossing + dest.height) && !self.hitTest(self.boundary - CHARACTER_WIDTH, self.crossing + dest.height))
+                (self.hitTest(self.boundary, self.crossing + self.dest.height) && !self.hitTest(self.boundary - CHARACTER_WIDTH_HALF, self.crossing + self.dest.height))
                 )
         };
 
         self.hitTestLeft = function() {
             return (
-                (self.hitTest(self.boundary - CHARACTER_WIDTH, self.crossing)
+                (self.hitTest(self.boundary - CHARACTER_WIDTH_HALF, self.crossing)
                 && !self.hitTest(self.boundary, self.crossing))
 
-                || (self.hitTest(self.boundary - CHARACTER_WIDTH, self.crossing + dest.height)
-                && !self.hitTest(self.boundary, self.crossing + dest.height)))
+                || (self.hitTest(self.boundary - CHARACTER_WIDTH_HALF, self.crossing + self.dest.height)
+                && !self.hitTest(self.boundary, self.crossing + self.dest.height)))
         };
 
         self.hitTestBottom = function() {
             return (
-                (self.hitTest(self.crossing, self.boundary) && !self.hitTest(self.crossing, self.boundary - CHARACTER_HEIGHT))
+                (self.hitTest(self.crossing, self.boundary) && !self.hitTest(self.crossing, self.boundary - CHARACTER_HEIGHT_HALF))
                 ||
-                (self.hitTest(self.crossing + dest.width, self.boundary) && !self.hitTest(self.crossing + dest.width, self.boundary - CHARACTER_HEIGHT))
+                (self.hitTest(self.crossing + self.dest.width, self.boundary) && !self.hitTest(self.crossing + self.dest.width, self.boundary - CHARACTER_HEIGHT_HALF))
                 )
         };
 
         self.hitTestTop = function() {
             return (
-                (self.hitTest(self.crossing, self.boundary - CHARACTER_HEIGHT) && !self.hitTest(self.crossing, self.boundary))
+                (self.hitTest(self.crossing, self.boundary - CHARACTER_HEIGHT_HALF) && !self.hitTest(self.crossing, self.boundary))
                 ||
-                (self.hitTest(self.crossing + dest.width, self.boundary - CHARACTER_HEIGHT) && !self.hitTest(self.crossing + dest.width, self.boundary))
+                (self.hitTest(self.crossing + self.dest.width, self.boundary - CHARACTER_HEIGHT_HALF) && !self.hitTest(self.crossing + self.dest.width, self.boundary))
                 )
         };
 
         self.wantsToMoveRight = function() {
-            return self.centerx > 0 && Math.floor(dest.right / CHARACTER_WIDTH) != Math.floor((dest.right - self.centerx) / CHARACTER_WIDTH);
+            return self.centerx > 0 && Math.floor(self.dest.right / CHARACTER_WIDTH_HALF) != Math.floor((self.dest.right - self.centerx) / CHARACTER_WIDTH_HALF);
         };
 
         self.wantsToMoveLeft = function() {
-            return self.centerx < 0 && Math.floor(dest.x / CHARACTER_WIDTH) != Math.floor((dest.x - self.centerx) / CHARACTER_WIDTH);
+            return self.centerx < 0 && Math.floor(self.dest.x / CHARACTER_WIDTH_HALF) != Math.floor((self.dest.x - self.centerx) / CHARACTER_WIDTH_HALF);
         };
 
         self.wantsToMoveUp = function() {
-            return self.centery < 0 && Math.floor(dest.y / CHARACTER_HEIGHT) != Math.floor((dest.y - self.centery) / CHARACTER_HEIGHT);
+            return self.centery < 0 && Math.floor(self.dest.y / CHARACTER_HEIGHT_HALF) != Math.floor((self.dest.y - self.centery) / CHARACTER_HEIGHT_HALF);
         };
 
         self.wantsToMoveDown = function()  {
-            return self.centery > 0 && Math.floor(dest.bottom / CHARACTER_HEIGHT) != Math.floor((dest.bottom - self.centery) / CHARACTER_HEIGHT)
+            return self.centery > 0 && Math.floor(self.dest.bottom / CHARACTER_HEIGHT_HALF) != Math.floor((self.dest.bottom - self.centery) / CHARACTER_HEIGHT_HALF)
         };
 
         self.updateBoundingBox = function() {
             var x = self.currentX + self.incx + OFFSET_X;
             var y = self.currentY + self.incy + OFFSET_Y;
-            var w = self.spriteWidth - (OFFSET_X * 2);
-            var h = self.spriteHeight - OFFSET_Y;
-            dest = new App.GameEngine.Rectangle(x, y, w, h);
+            var w = (CHARACTER_WIDTH_HALF * 2) - (OFFSET_X * 2);
+            var h = (CHARACTER_HEIGHT_HALF * 2) - OFFSET_Y;
+            self.dest = new App.GameEngine.Rectangle(x, y, w, h);
         };
     };
 
