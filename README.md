23c5cbe9f682a24eee4dd852d00f9363c4b8043b
Bram De Moor <bram.demoor@gmail.com>
2013-01-06 13:51:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Sandbox-NodeJS
+==============
+
+Multiplayer platform game proof of concept.
+
+Technologies used:  
+- KnockoutJS (UI bindings)     
+- Twitter Bootstrap (site layout)  
+- EnchantJS (Game canvas)  
+- NodeJS (Server)  
+- SocketIO (Websockets)
---
+++ server.js
@@ -0,0 +1,59 @@
+var http = require('http'),
+    fs = require('fs');
+
+var server = http.createServer(function (req, res) {
+  fs.readFile(__dirname + "/index.html", function (err, data) {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      return res.end("Error");
+    }
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+  });
+});
+
+server.listen(8080);
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket) {
+  var state = {
+    sequence: Math.floor(Math.random() * (2 << 16)),
+    remain: 10,
+    time: null
+  }
+
+  var id = setInterval(function() {
+    if (state.remain === 0) {
+      clearInterval(id);
+      return;
+    }
+
+    state.time = new Date();
+    socket.emit('ping', { seq: state.sequence });
+    state.remain -= 1;
+  }, 1000);
+
+  socket.on('pong', receivePing.bind(null, state));
+});
+
+function receivePing (state, data) {
+  var clientSeq = data["seq"]
+  if (clientSeq != state.sequence + 1 || state.time === null) {
+    dropPingReceive(state);
+    return;
+  }
+
+  var currentTime = new Date();
+  var diff = currentTime.getTime() - state.time.getTime();
+  
+  console.log("Received packet with RTT of %d ms", diff)
+
+  state.sequence += 2;
+  state.time = null;
+}
+
+function dropPingReceive (state) {
+  console.warn("Dropped packet with number %d", state.sequence);
+}
