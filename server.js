64b93f9a347c8fa83cc17318358e9bd7f0761d3b
Bram De Moor <bram.demoor@gmail.com>
2013-01-09 21:26:33 +0100

    Player disconnects

--- server.js
+++ server.js
@@ -18,18 +18,26 @@ app.get('/activeLevel', function(req, res) {
 var players = [];
 
 app.io.route('join', function(req) {
-    players.push({ name : req.data.name, x: 0, y: 0, health: 100, score: 0, upPressed: false, leftPressed: false, rightPressed: false });
-    console.log('Player joined: ' + req.data.name);
+    players.push({ socketid: req.socket.id, name : req.data.name, x: 0, y: 0, health: 100, score: 0, upPressed: false, leftPressed: false, rightPressed: false });
+    console.log('Player joined: ' + req.data.name + ' (id: ' + req.socket.id + ')');
     req.io.emit('joinSuccess', req.data)
 });
 
-app.io.route('disconnect', function() {
-    console.log('USER DISCONNECTED');
+app.io.route('disconnect', function(req) {
+    console.log('USER DISCONNECTED: ' + req.socket.id);
+
+    // Remove this player
+    for(var i = 0; i < players.length; i++) {
+        if(players[i].socketid == req.socket.id) {
+            players.splice(i,1);
+            console.log('REMOVED PLAYER');
+        }
+    }
 });
 
 app.io.route('moved', function(req) {
     for(var i = 0; i < players.length; i++) {
-        if(players[i].name == req.data.name) {
+        if(players[i].socketid == req.socket.id) {
             players[i].x = req.data.x;
             players[i].y = req.data.y;
             players[i].health = req.data.health;
